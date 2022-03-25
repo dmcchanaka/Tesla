7a753da4daa77678fae04ecbf5f76676ebfd432e
Chaminda Chanaka <chanaka@itplace.com>
2022-03-25 13:45:53 +0530

    complete header

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const image = { uri: "https://reactjs.org/logo-og.png" };
 
+import Header from './components/header/index';
 import CarList from './components/CarList/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarList />
       <StatusBar style="auto" />
     </View>
--- components/CarList/index.js
+++ components/CarList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Dimensions } from 'react-native';
 import cars from './cars';
 import CarContainer from '../CarItem/index';
 import styles from './style';
@@ -11,6 +11,10 @@ const CarList = () => {
             data={cars} 
             renderItem={({item}) => <CarContainer car={item} /> }    
             keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+            snapToAlignment={'start'}
+            decelerationRate={'fast'}
+            snapToInterval={Dimensions.get('screen').height}
             />
         </View>
         
